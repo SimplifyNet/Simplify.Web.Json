a1081adb4bd9a667c5fa9e50ad8869cd52e14a55
Alexanderius <alexander.krylkov@gmail.com>
2016-08-23 14:55:23 +0600

    Implemented from Simplify.Web 0.9 using Newtonsoft.Json 9.0.1

---
+++ Simplify.Web.Json/Responses/Json.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace Simplify.Web.Json.Responses
+{
+	/// <summary>
+	/// Provides controller JSON response (send only JSON string to response)
+	/// </summary>
+	public class Json : ControllerResponse
+	{
+		private readonly object _objectToConvert;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Json"/> class.
+		/// </summary>
+		/// <param name="objectToConvert">The object to convert to JSON.</param>
+		public Json(object objectToConvert)
+		{
+			_objectToConvert = objectToConvert;
+		}
+
+		/// <summary>
+		/// Processes this response
+		/// </summary>
+		public override ControllerResponseResult Process()
+		{
+			Context.Response.ContentType = "application/json";
+
+			ResponseWriter.Write(JsonConvert.SerializeObject(_objectToConvert), Context.Response);
+
+			return ControllerResponseResult.RawOutput;
+		}
+	}
+}
