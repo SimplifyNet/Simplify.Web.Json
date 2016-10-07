0eb53748894da7a5245c809410f01d659223df21
Alexanderius <alexander.krylkov@gmail.com>
2016-10-07 14:28:08 +0600

    Fixes #2, upgrade to Simplify.Web 1.0.1

--- Simplify.Web.Json/Responses/Json.cs
+++ Simplify.Web.Json/Responses/Json.cs
@@ -10,6 +10,14 @@ namespace Simplify.Web.Json.Responses
 		private readonly object _objectToConvert;
 
 		/// <summary>
+		/// Gets the HTTP response status code.
+		/// </summary>
+		/// <value>
+		/// The HTTP response status code.
+		/// </value>
+		private readonly int _statusCode;
+
+		/// <summary>
 		/// Initializes a new instance of the <see cref="Json"/> class.
 		/// </summary>
 		/// <param name="objectToConvert">The object to convert to JSON.</param>
@@ -19,11 +27,24 @@ namespace Simplify.Web.Json.Responses
 		}
 
 		/// <summary>
+		/// Initializes a new instance of the <see cref="Json"/> class.
+		/// </summary>
+		/// <param name="objectToConvert">The object to convert to JSON.</param>
+		/// <param name="statusCode">The HTTP response status code.</param>
+		public Json(object objectToConvert, int statusCode = 200)
+		{
+			_objectToConvert = objectToConvert;
+			_statusCode = statusCode;
+		}
+
+		/// <summary>
 		/// Processes this response
 		/// </summary>
+		/// <returns></returns>
 		public override ControllerResponseResult Process()
 		{
 			Context.Response.ContentType = "application/json";
+			Context.Response.StatusCode = _statusCode;
 
 			ResponseWriter.Write(JsonConvert.SerializeObject(_objectToConvert), Context.Response);
 
