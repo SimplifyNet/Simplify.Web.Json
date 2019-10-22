dfb6319a6b8a5e3a293ce4c2555490a0a680a34f
Alexanderius <alexander.krylkov@gmail.com>
2019-10-22 21:54:56 +0600

    Fixes #7

--- Simplify.Web.Json/Model/Binding/JsonModelBinder.cs
+++ Simplify.Web.Json/Model/Binding/JsonModelBinder.cs
@@ -2,7 +2,7 @@
 using Simplify.Web.Model.Binding;
 using Simplify.Web.Model.Validation;
 
-namespace Simplify.Web.Json.ModelBinding.Binders
+namespace Simplify.Web.Json.Model.Binding
 {
 	/// <summary>
 	/// Provides HTTP request JSON data to object binding
