864bff9a914838976e24120d1ce14c29b105e00c
Alexanderius <alexander.krylkov@gmail.com>
2023-11-06 16:10:53 +0600

    [add] Array and IList controllers

---
+++ src/TesterApp/Controllers/Api/v1/TestInArrayController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Simplify.Web;
+using Simplify.Web.Attributes;
+using TesterApp.ViewModels;
+
+namespace TesterApp.Controllers.Api.v1;
+
+[Post("/api/v1/test-in-array")]
+public class TestInArrayController : AsyncController<TestViewModel[]>
+{
+	public override async Task<ControllerResponse> Invoke()
+	{
+		await ReadModelAsync();
+
+		Console.WriteLine($"Objects count: {Model.Length}");
+
+		for (var i = 0; i < Model.Length; i++)
+			Console.WriteLine($"Object {i} prop1: {Model[i].Prop1}");
+
+		return NoContent();
+	}
+}
---
+++ src/TesterApp/Controllers/Api/v1/TestInIListController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Simplify.Web;
+using Simplify.Web.Attributes;
+using TesterApp.ViewModels;
+
+namespace TesterApp.Controllers.Api.v1;
+
+[Post("/api/v1/test-in-ilist")]
+public class TestInIListController : AsyncController<IList<TestViewModel>>
+{
+	public override async Task<ControllerResponse> Invoke()
+	{
+		await ReadModelAsync();
+
+		Console.WriteLine($"Objects count: {Model.Count}");
+
+		for (var i = 0; i < Model.Count; i++)
+			Console.WriteLine($"Object {i} prop1: {Model[i].Prop1}");
+
+		return NoContent();
+	}
+}
